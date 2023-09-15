24099f6accaf16c9eeecf0607baa5b8c9cfc8b63
Sushmita Ghosh <pujaghosh251096@gmail.com>
2023-09-16 00:21:57 +0530

    understanding the useState hook by building a filter func

--- src/components/RestaurantContainer/RestaurantContainer.js
+++ src/components/RestaurantContainer/RestaurantContainer.js
@@ -3,13 +3,29 @@ import ReactDOM from "react-dom";
 import "./RestaurantContainer.css";
 import RestaurantCard from "../RestaurantCard/RestaurantCard";
 import restaurants from "../../utils/mock-data";
+import { useState } from "react";
 
 const RestaurantContainer = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+
   return (
     <div className="restaurant-container">
-      <div className="search">Search</div>
+      {/* <div className="search">Search</div> */}
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (restaurant) => restaurant.info.rating.aggregate_rating >= 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Resturants
+        </button>
+      </div>
       <div className="res-cards">
-        {restaurants.map((restaurant) => {
+        {listOfRestaurants.map((restaurant) => {
           return <RestaurantCard restaurant={restaurant} />;
         })}
       </div>
