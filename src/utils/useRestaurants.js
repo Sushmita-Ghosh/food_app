932a0cf7517053dec7715917d49035352dd22f37
Sushmita Ghosh <pujaghosh251096@gmail.com>
2023-10-01 21:18:18 +0530

    Seperating logic for restaurants fetching seperately using custom hook

---
+++ src/utils/useRestaurants.js
@@ -0,0 +1,43 @@
+// This is a custom hook - we are extracting the logic of fetching restaurants from Swiggy API
+//  we will set the value of filteredRestaurants and listofRestaurants
+
+import { useEffect, useState } from "react";
+
+const useRestaurants = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  // fetching the restaurants
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    // console.log(
+    //   json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    // );
+
+    setListOfRestaurants(
+      // Optional Chaining
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    setFilteredRestaurants(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return {
+    filteredRestaurants,
+    listOfRestaurants,
+    setFilteredRestaurants,
+  };
+};
+
+export default useRestaurants;
